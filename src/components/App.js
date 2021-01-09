d7ce9a37aeb3be39a0f24c061a647aa4a59a12eb
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-09 18:35:24 +0000

    Init

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import binance from "../components/api/binance";
 
 const App = () => {
-  return <div className="ui container">Hi there</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function search() {
+      const response = await binance.get("/time"); // means i get the data property from the response
+      console.log(response);
+      //setData(response);
+    }
+
+    search();
+  }, []);
+
+  return <div className="ui container">{}</div>;
 };
 
 export default App;
+
+//create dropdown - on dropdown, populate it with all cryptocurrencies
