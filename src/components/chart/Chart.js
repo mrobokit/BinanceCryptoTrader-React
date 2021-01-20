e2077ba8456a9522eb148de10c0a1f3cde0512dd
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-20 13:47:50 +0000

    Clean Heikin Ashi

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,56 +1,99 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { scaleTime } from "d3-scale";
-import { utcDay } from "d3-time";
+import { format } from "d3-format";
+import { timeFormat } from "d3-time-format";
 
 import { ChartCanvas, Chart } from "react-stockcharts";
-import { CandlestickSeries } from "react-stockcharts/lib/series";
+import {
+  BarSeries,
+  AreaSeries,
+  CandlestickSeries,
+  LineSeries,
+} from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
-import { fitWidth } from "react-stockcharts/lib/helper";
-import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
+import {
+  CrossHairCursor,
+  EdgeIndicator,
+  CurrentCoordinate,
+  MouseCoordinateX,
+  MouseCoordinateY,
+} from "react-stockcharts/lib/coordinates";
 
-class CandleStickChart extends React.Component {
+import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
+import {
+  OHLCTooltip,
+  MovingAverageTooltip,
+} from "react-stockcharts/lib/tooltip";
+import { ema, heikinAshi, sma } from "react-stockcharts/lib/indicator";
+import { fitWidth } from "react-stockcharts/lib/helper";
+import { last } from "react-stockcharts/lib/utils";
+
+class HeikinAshi extends React.Component {
   render() {
-    const { type, width, data, ratio } = this.props;
-    const xAccessor = (d) => d?.date;
-    const xExtents = [
-      xAccessor(last(data)),
-      xAccessor(data[data.length - 100]),
-    ];
+    const ha = heikinAshi();
+
+    const { type, data: initialData, width, ratio } = this.props;
+
+    const calculatedData = ha(initialData);
+    const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor(
+      (d) => d.date
+    );
+    const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(
+      calculatedData
+    );
+
+    const start = xAccessor(last(data));
+    const end = xAccessor(data[Math.max(0, data.length - 150)]);
+    const xExtents = [start, end];
+
     return (
       <ChartCanvas
         height={400}
         ratio={ratio}
         width={width}
-        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+        margin={{ left: 80, right: 80, top: 10, bottom: 30 }}
         type={type}
         seriesName="MSFT"
         data={data}
+        xScale={xScale}
         xAccessor={xAccessor}
-        xScale={scaleTime()}
+        displayXAccessor={displayXAccessor}
         xExtents={xExtents}
       >
-        <Chart id={1} yExtents={(d) => [d.high, d.low]}>
-          <XAxis axisAt="bottom" orient="bottom" ticks={6} />
-          <YAxis axisAt="left" orient="left" ticks={5} />
-          <CandlestickSeries width={timeIntervalBarWidth(utcDay)} />
+        <Chart
+          id={1}
+          yExtents={[(d) => [d.high, d.low]]}
+          padding={{ top: 10, bottom: 20 }}
+        >
+          <XAxis axisAt="bottom" orient="bottom" />
+          <YAxis axisAt="right" orient="right" ticks={5} />
+          <MouseCoordinateY
+            at="right"
+            orient="right"
+            displayFormat={format(".1f")}
+          />
+
+          <CandlestickSeries />
         </Chart>
+
+        <CrossHairCursor />
       </ChartCanvas>
     );
   }
 }
 
-CandleStickChart.propTypes = {
+HeikinAshi.propTypes = {
   data: PropTypes.array.isRequired,
   width: PropTypes.number.isRequired,
   ratio: PropTypes.number.isRequired,
   type: PropTypes.oneOf(["svg", "hybrid"]).isRequired,
 };
 
-CandleStickChart.defaultProps = {
+HeikinAshi.defaultProps = {
   type: "svg",
 };
-CandleStickChart = fitWidth(CandleStickChart);
 
-export default CandleStickChart;
+HeikinAshi = fitWidth(HeikinAshi);
+
+export default HeikinAshi;
