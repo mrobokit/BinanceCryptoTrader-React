718c7b54c263abf272ef2978a648c0d3a066c598
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-22 16:27:02 +0000

    Added Netlify Identity and Netlify CLI globally

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,11 @@ import Navbar from "../components/Navbar";
 import "../css/App.css";
 import SymbolPage from "../pages/SymbolPage";
 import SettingsPage from "../pages/SettingsPage";
+import { IdentityContextProvider } from "react-netlify-identity";
 
 const App = () => {
+  //const url = "https://your-identity-instance.netlify.com/"; // supply the url of your Netlify site instance with Identity enabled. VERY IMPORTANT
+
   return (
     <BrowserRouter>
       <div className="ui container container-style hundredvh">
